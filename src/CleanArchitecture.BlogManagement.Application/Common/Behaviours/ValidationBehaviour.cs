8ec826034f22ca2bb00b1d23c5914c9780632276
Emrul Kayes <emrul@blockbonds.io>
2024-01-15 11:35:54 +0600

    add serilog with file writter

---
+++ src/CleanArchitecture.BlogManagement.Application/Common/Behaviours/ValidationBehaviour.cs
@@ -0,0 +1,11 @@
+﻿using MediatR;
+
+namespace CleanArchitecture.BlogManagement.Application.Common.Behaviours;
+public sealed class ValidationBehaviour<TRequest, TResponse> :  IPipelineBehavior<TRequest, TResponse>
+where TRequest : notnull
+{
+    public Task<TResponse> Handle(TRequest request, RequestHandlerDelegate<TResponse> next, CancellationToken cancellationToken)
+    {
+        throw new NotImplementedException();
+    }
+}
---
+++ src/CleanArchitecture.BlogManagement.WebApi/DependencyInjection.cs
@@ -0,0 +1,15 @@
+﻿using Serilog;
+
+namespace CleanArchitecture.BlogManagement.WebApi;
+
+public static class DependencyInjection
+{
+    public static ConfigureHostBuilder RegisterSerilog(this ConfigureHostBuilder builder)
+    {
+        builder.UseSerilog((context, loggerConfiguration) =>
+        {
+            loggerConfiguration.ReadFrom.Configuration(context.Configuration);
+        });
+        return builder;
+    }
+}
