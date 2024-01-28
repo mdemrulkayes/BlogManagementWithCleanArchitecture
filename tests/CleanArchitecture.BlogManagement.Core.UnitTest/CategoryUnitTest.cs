44a6f48960adefe57ad334f0f4a9b092762dee4d
Emrul Kayes <emrul.kayes2103@gmail.com>
2024-01-28 22:24:04 +0600

    add unit test project to test the Core layer

---
+++ tests/CleanArchitecture.BlogManagement.Core.UnitTest/CategoryUnitTest.cs
@@ -0,0 +1,43 @@
+﻿using CleanArchitecture.BlogManagement.Core.Base;
+
+namespace CleanArchitecture.BlogManagement.Core.UnitTest;
+public sealed class CategoryUnitTest
+{
+    [Theory]
+    [InlineData("Test", "Test description")]
+    public void Create_Category_Should_Return_CategoryObjectWithResult(string categoryName, string description)
+    {
+        //Arrange
+
+        //Act
+
+        var category = Category.Category.Create(categoryName, description);
+
+        //Assert
+
+        Assert.True(category.IsSuccess);
+        Assert.NotNull(category.Value);
+        Assert.IsType<Result<Category.Category>>(category);
+    }
+
+    [Fact]
+    public void Update_Category_Name_Should_Return_CategoryUpdatedObjectWithResult()
+    {
+        //Arrange
+        var category = Category.Category.Create("Test", "Test Description");
+        //Assert
+        Assert.True(category.IsSuccess);
+        Assert.NotNull(category.Value);
+        //Act
+
+        var updatedCategory = category.Value.Update("Test Update", "Test Description");
+
+        //Assert
+
+        Assert.True(updatedCategory.IsSuccess);
+        Assert.NotNull(updatedCategory.Value);
+        Assert.IsType<Result<Category.Category>>(updatedCategory);
+        Assert.Equal(updatedCategory.Value.CategoryId, category.Value.CategoryId);
+
+    }
+}
