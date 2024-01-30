2caf88390f5d90fcc33544c32a04dafb907bc3f1
Emrul Kayes <emrul.kayes2103@gmail.com>
2024-01-30 16:07:06 +0600

    Implement interface segragation principle in Base repository

---
+++ src/CleanArchitecture.BlogManagement.Core/Base/IReadRepository.cs
@@ -0,0 +1,18 @@
+﻿using System.Linq.Expressions;
+
+namespace CleanArchitecture.BlogManagement.Core.Base;
+public interface IReadRepository<TEntity> where TEntity : BaseEntity
+{
+    Task<TEntity?> FirstOrDefaultAsync(
+        Expression<Func<TEntity, bool>> expression,
+        string includeProperties = ""
+    );
+
+    Task<List<TEntity>> FindAllAsync(
+        Expression<Func<TEntity, bool>>? expression = null,
+        CancellationToken cancellationToken = default
+    );
+    Task<bool> AnyAsync(
+        Expression<Func<TEntity, bool>> expression
+    );
+}
--- src/CleanArchitecture.BlogManagement.Core/Base/IRepository.cs
+++ src/CleanArchitecture.BlogManagement.Core/Base/IRepository.cs
@@ -1,7 +1,8 @@
 ﻿using System.Linq.Expressions;
 
 namespace CleanArchitecture.BlogManagement.Core.Base;
-public interface IRepository<TEntity>  where TEntity : BaseEntity
+public interface IRepository<TEntity> : IReadRepository<TEntity>
+    where TEntity : BaseEntity
 {
     Task<TEntity> Add(TEntity entity);
     Task<TEntity> Update(TEntity entity);
@@ -9,17 +10,4 @@ public interface IRepository<TEntity>  where TEntity : BaseEntity
     Task Delete(TEntity entity);
 
     Task Delete(Expression<Func<TEntity, bool>> expression);
-
-    Task<TEntity?> FirstOrDefaultAsync(
-        Expression<Func<TEntity, bool>> expression,
-        string includeProperties = ""
-    );
-
-    Task<List<TEntity>> FindAllAsync(
-        Expression<Func<TEntity, bool>>? expression = null,
-        CancellationToken cancellationToken = default
-    );
-    Task<bool> AnyAsync(
-        Expression<Func<TEntity, bool>> expression
-        );
 }
